1aecc541d500034ae7e7fadcda8c182ed9897401
CoimbraDouglas <coimbra.doglas@hotmail.com>
2025-09-18 21:39:22 -0300

    Upload do projeto de aplicação web com H2

---
+++ src/main/java/com/example/demo/services/DepartamentServices.java
@@ -0,0 +1,33 @@
+package com.example.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.model.Departament;
+import com.example.demo.repository.DepartamentRepository;
+
+
+@Service
+public class DepartamentServices {
+
+    @Autowired
+    private DepartamentRepository departmentRepository;
+
+public List<Departament> findAll() {
+    return departmentRepository.findAll();
+}
+
+
+    public Optional<Departament> findById(Long id) {
+        return departmentRepository.findById(id);
+}
+    public Departament save(Departament department) {
+        return departmentRepository.save(department);
+    }
+
+    public void deleteById(Long id) {
+        departmentRepository.deleteById(id);
+    }
+
+}
